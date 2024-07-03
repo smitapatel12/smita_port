93fc5841b63d2df47c59503c03ccae55b720d86f
smitapatel12 <110783386+smitapatel12@users.noreply.github.com>
2024-07-03 08:38:34 +0000

    i change

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Smita Patel",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "spatel.mgs1206@gmail.com",
+  gitHub: "https://github.com/smitapatel12",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/smita-patel-a78064230",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "educationaladda",
 };
 
 const primaryColor = "#4E567E";
